c21fbdb237014ccfe64be303376519836056f8ae
Nicholas Outram <nicholas.outram@plymouth.ac.uk>
2013-12-03 09:51:50 +0000

    Tweak to comments

--- FarmKrappApp/SoCMViewControllerCommonBase.h
+++ FarmKrappApp/SoCMViewControllerCommonBase.h
@@ -5,6 +5,9 @@
 //  Created by Nicholas Outram on 03/12/2013.
 //  Copyright (c) 2013 Plymouth University. All rights reserved.
 //
+// COMMON TO MANY/ALL VIEW CONTROLLERS
+// Provides:
+//  Accessor to Singleton CoreData managedobjectcontext
 
 #import <UIKit/UIKit.h>
 
