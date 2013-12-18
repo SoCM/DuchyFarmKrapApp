50060b570e11b5db5df803daf52179ee1491ff56
Nicholas Outram <nicholas.outram@plymouth.ac.uk>
2013-12-18 06:17:15 +0000

    Single cell for manure type and quality

---
+++ FarmKrappApp/FCAManureCell.h
@@ -0,0 +1,17 @@
+//
+//  FCAManureCell.h
+//  FarmKrappApp
+//
+//  Created by Nicholas Outram on 18/12/2013.
+//  Copyright (c) 2013 Plymouth University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FCAManureCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *manureTypeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *manureQualityLabel;
+
+
+@end
---
+++ FarmKrappApp/FCAManureCell.m
@@ -0,0 +1,29 @@
+//
+//  FCAManureCell.m
+//  FarmKrappApp
+//
+//  Created by Nicholas Outram on 18/12/2013.
+//  Copyright (c) 2013 Plymouth University. All rights reserved.
+//
+
+#import "FCAManureCell.h"
+
+@implementation FCAManureCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
