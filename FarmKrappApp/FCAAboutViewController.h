61816ff7c62743cae16e8507351018e4c75e7294
Nicholas Outram <nicholas.outram@plymouth.ac.uk>
2014-02-02 11:21:32 +0000

    About page for FCA

---
+++ FarmKrappApp/FCAAboutViewController.h
@@ -0,0 +1,15 @@
+//
+//  FCAAboutViewController.h
+//  FarmKrappApp
+//
+//  Created by Nicholas Outram on 02/02/2014.
+//  Copyright (c) 2014 Plymouth University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FCAAboutViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIWebView *webview;
+- (IBAction)doBack:(id)sender;
+
+@end
